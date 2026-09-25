4e7a38cc71ebd444326ffc3b4010aa3cdc3d09fa
Samuel Recalde <1107848923@u.icesi.edu.co>
2026-09-25 17:50:35 -0500

    Implementación de QuickSort y Pruebas

--- src/main/scala/QuickSort.scala
+++ src/main/scala/QuickSort.scala
@@ -1,25 +1,48 @@
+import scala.annotation.tailrec
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-@main
-def QuickSort(): Unit = {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  (1 to 5).map(println)
+object QuickSort {
+  def menores(inputList: List[Int], p: Int): List[Int] = {
+    inputList match {
+      case Nil => Nil
+      case head :: tail =>
+        if head < p then
+          head :: menores(tail, p)
+        else
+          menores(tail, p)
+    }
+  }
 
-  for (i <- 1 to 5) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    println(s"i = $i")
+  def mayores(inputList: List[Int], p: Int): List[Int] = {
+    inputList match {
+      case Nil => Nil
+      case head :: tail =>
+        if head >= p then
+          head :: mayores(tail, p)
+        else
+          mayores(tail, p)
+    }
+  }
+
+  def appendTR(inputL1: List[Int], inputL2: List[Int]): List[Int] =
+    inputL1 match
+      case Nil => inputL2
+      case head :: tail =>
+        head :: appendTR(tail, inputL2)
+
+  @tailrec
+  def separar(inputList: List[Int], p: Int, menoresAcc: List[Int], mayoresAcc: List[Int]): (List[Int], List[Int]) =
+    inputList match
+      case Nil => (menoresAcc, mayoresAcc)
+      case head :: tail =>
+        if head < p then
+          separar(tail, p, appendTR(menoresAcc, List(head)), mayoresAcc)
+        else
+          separar(tail, p, menoresAcc, appendTR(mayoresAcc, List(head)))
+
+  def quickSort(inputList: List[Int]): List[Int] = {
+    inputList match
+      case Nil => Nil
+      case x :: Nil => List(x)
+      case p :: t => appendTR(appendTR(quickSort(menores(t, p)), List(p)), quickSort(mayores(t, p)))
   }
 }
-
-def menores(inputList: List[Int], p: Int): List[Int]
-def mayores(inputList: List[Int], p: Int): List[Int]
-def appendTR(inputL1: List[Int], inputL2: List[Int]): List[Int]
-def separar(
-                inputList: List[Int],
-                p: Int,
-                menoresAcc: List[Int],
-                mayoresAcc: List[Int]
-              ): (List[Int], List[Int])
-def quickSort(inputList: List[Int]): List[Int]
--- src/test/scala/QuickSortSuite.scala
+++ src/test/scala/QuickSortSuite.scala
@@ -1,5 +1,50 @@
-menores
-mayores
-appendTR
-separar
-quickSort
+import munit.FunSuite
+
+class QuickSortSuite extends FunSuite:
+
+  test("menores: Devuelve los elementos menores al pivote"):
+    val resultado = QuickSort.menores(List(8, 3, 10, 2, 5), 6)
+    assertEquals(resultado, List(3, 2, 5))
+
+  test("menores: Devuelve una lista vacía si no hay elementos menores"):
+    val resultado = QuickSort.menores(List(8, 10, 7), 5)
+    assertEquals(resultado, Nil)
+
+  test("mayores: Devuelve los elementos mayores que el pivote"):
+    val resultado = QuickSort.mayores(List(8, 3, 10, 2, 5), 6)
+    assertEquals(resultado, List(8, 10))
+
+  test("mayores: Devuelve una lista vacía si no hay elementos mayores"):
+    val resultado = QuickSort.mayores(List(3, 5, 2), 5)
+    assertEquals(resultado, Nil)
+
+  test("appendTR: concatena dos listas"):
+    assertEquals(QuickSort.appendTR(List(1, 2, 3), List(4, 5)), List(1, 2, 3, 4, 5))
+
+  test("appendTR: funciona con una lista vacía"):
+    assertEquals(QuickSort.appendTR(Nil, List(1, 2, 3)), List(1, 2, 3))
+
+  test("separar: divide la lista según el pivote"):
+    assertEquals(QuickSort.separar(List(8, 3, 10, 2, 5), 6, Nil, Nil), (List(3, 2, 5), List(8, 10)))
+
+  test("separar: coloca los iguales al pivote en mayores"):
+    assertEquals(QuickSort.separar(List(3, 5, 7, 5, 9), 5, Nil, Nil), (List(3), List(5, 7, 5, 9)))
+
+  test("separar: funciona con lista vacía"):
+    assertEquals(QuickSort.separar(Nil, 5, Nil, Nil), (Nil, Nil))
+
+  test("quickSort: ordena una lista desordenada"):
+    val resultado = QuickSort.quickSort(List(8, 3, 10, 2, 5))
+    assertEquals(resultado, List(2, 3, 5, 8, 10))
+
+  test("quickSort: funciona con lista vacía"):
+    val resultado = QuickSort.quickSort(Nil)
+    assertEquals(resultado, Nil)
+
+  test("quickSort: funciona con un solo elemento"):
+    val resultado = QuickSort.quickSort(List(7))
+    assertEquals(resultado, List(7))
+
+  test("quickSort: ordena una lista con elementos repetidos"):
+    val resultado = QuickSort.quickSort(List(5, 3, 5, 2, 3, 1))
+    assertEquals(resultado, List(1, 2, 3, 3, 5, 5))
