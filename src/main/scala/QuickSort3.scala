913eb34c93f3d359db29f80f17acc956ca82ec5a
alejoicaro11 <alejoicaro11@gmail.com>
2026-09-25 17:41:55 -0500

    Refactor quickSort3 implementation
    Refactor quickSort3 to use pattern matching and recursion for sorting.

--- src/main/scala/QuickSort3.scala
+++ src/main/scala/QuickSort3.scala
@@ -5,5 +5,18 @@ def separar3(
               menoresAcc: List[Int],
               igualesAcc: List[Int],
               mayoresAcc: List[Int]
-            ): (List[Int], List[Int], List[Int])
-def quickSort3(inputList: List[Int]): List[Int]
+            ): (List[Int], List[Int], List[Int]) =
+  inputList match
+    case Nil => (menoresAcc, igualesAcc, mayoresAcc)
+    case head :: tail => if head < p then
+      separar3(tail, p, head :: menoresAcc, igualesAcc, mayoresAcc) else
+      separar3(tail, p, menoresAcc, igualesAcc, head :: mayoresAcc)
+
+def quickSort3(inputList: List[Int]): List[Int] =
+  inputList match
+    case Nil => Nil
+    case x :: Nil => List(x)
+    case p :: t =>
+      val (men, ig, may) = separar3(t, p, Nil, Nil, Nil)
+        appendTR(quickSort(men), appendTR(p:: ig, quickSort3(may)))
+end quickSort3
